83bd7e65fe50a0b88360bfba861ea663212b10e0
haritowa <haritowa@icloud.com>
2018-05-26 17:08:45 +0300

    Add vapor paragraph

---
+++ tex/inc/src/vapor_db.swift
@@ -0,0 +1,22 @@
+final class User: Model {
+    typealias Database = FooDatabase
+    typealias ID = Int
+
+    static let idKey: IDKey = \.id
+
+    /// The unique identifier for this user.
+    var id: Int?
+
+    /// The user's full name.
+    var name: String
+
+    /// The user's current age in years.
+    var age: Int
+
+    /// Creates a new user.
+    init(id: Int? = nil, name: String, age: Int) {
+        self.id = id
+        self.name = name
+        self.age = age
+    }
+}
