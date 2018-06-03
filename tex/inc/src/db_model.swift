63204e0ff161975bb97c665c3a3115bdea10d052
haritowa <haritowa@icloud.com>
2018-06-04 01:17:34 +0300

    Update listing

--- tex/inc/src/db_model.swift
+++ tex/inc/src/db_model.swift
@@ -1,11 +1,22 @@
 class DeviceModel: Object {
+    // Device id, must be unique
     dynamic var id: String = ""
+
+    // Device name
     dynamic var name: String = ""
 
     // Decoded key data
     dynamic var publicKey: Data = Data()
 
+    @objc dynamic var tmpID = 0
+
+    let owners = LinkingObjects(fromType: User.self, property: "devices")
+
     @objc override class func primaryKey() -> String? {
         return "id"
+    }
+
+    override static func ignoredProperties() -> [String] {
+        return ["tmpID"]
     }
 }
