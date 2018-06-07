ac240561b286e09461a0962b92bfee35a3f9b880
haritowa <haritowa@icloud.com>
2018-06-07 08:51:35 +0300

    WIP

--- tex/inc/src/vc.swift
+++ tex/inc/src/vc.swift
@@ -4,3 +4,27 @@ public protocol ViewModelInjectable {
     func setup(with viewModel: AssociatedViewModel)
     func generateInput() -> AssociatedViewModel.InitInput
 }
+
+extension ViewModelInjectable {
+    fileprivate func setup(using vm: AssociatedViewModel) {
+        vm.setup(with: generateInput())
+        setup(with: vm)
+    }
+}
+
+extension ViewModelInjectable where Self: UIViewController {
+    func inject(viewModel: AssociatedViewModel) -> Disposable {
+        guard !isViewLoaded else {
+            setup(using: viewModel)
+            return
+        }
+        
+        let rxSelf = self as UIViewController
+        
+        return rxSelf.rx.sentMessage(#selector(UIViewController.viewDidLoad))
+            .map { _ in viewModel }
+            .subscribe(onNext: { [unowned self] (_) in
+                self.setup(using: viewModel)
+            })
+    }
+}
